be7030490288f31786c6c3ee19ee07d140c03ba4
kolmstead17 <22797420+kolmstead17@users.noreply.github.com>
2024-10-31 12:19:23 -0400

    Initial commit

---
+++ EXE_Deletion.ps1
@@ -0,0 +1,43 @@
+#Get-ChildItem -Path 'C:\$Recycle.Bin' -Force | Remove-Item -Recurse -Force
+
+$users = Get-ChildItem C:\Users
+
+foreach ($user in $users){
+    $folder = "C:\users\" + $user + "\downloads"
+    $folderPath = Test-Path -Path $folder
+    $noFiles = "No files found that meet criteria for $user"
+
+    if($folderPath){
+        $files = Get-ChildItem $folder -Filter *.exe | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-365))}
+        $totalSize = ( $files | Measure-Object -Sum Length).Sum/1GB
+        
+        if ($files.Length -ne 0){
+            Write-Output "$($files.Length) file(s) found for $user. Total Size = $($totalSize)GB" #Last write time(s) = $($files.Length)"
+            $answer = Read-Host "Would you like to delete these files? (y/n)"
+            
+            try{
+                if($answer -eq "y"){
+                    foreach($file in $files){
+                        Get-ChildItem $folder -Filter *.exe | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-365))} | Select-Object -ExpandProperty FullName | ForEach-Object {Remove-Item -Path $_ -Force -Recurse -Verbose}
+                }
+                
+                    Write-Output "File(s) deleted"
+                }
+
+                else{
+                    Write-Output "File(s) not deleted"
+                }
+            }
+            catch{
+                Write-Output $Error
+            }
+        }
+        else{
+            Write-Output $noFiles
+        }
+    }
+
+    else {
+        Write-Output $noFiles
+    }
+}
---
+++ OST_Deletion.ps1
@@ -0,0 +1,40 @@
+$users = Get-ChildItem C:\Users
+
+foreach ($user in $users){
+    $folder = "C:\users\" + $user + "\appdata\Local\Microsoft\Outlook"
+    $folderPath = Test-Path -Path $folder
+    $noFiles = "No files found that meet criteria for $user"
+
+    if($folderPath){
+        $files = Get-ChildItem $folder -Filter *.ost | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-182))} 
+        
+        if ($files.Length -ne 0){
+            Write-Output "$($files.Name) file(s) found for $user. Last write time(s) = $($files.LastWriteTime)"
+            $answer = Read-Host "Would you like to delete these files? (y/n)"
+
+            try{
+                if($answer -eq "y"){
+                    foreach($file in $files){
+                        Get-ChildItem $folder -Filter *.ost | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-182))} | Select-Object -ExpandProperty FullName | ForEach-Object {Remove-Item -Path $_ -Force -Recurse -Verbose}
+                }
+                
+                    Write-Output "File(s) deleted"
+                }
+
+                else{
+                    Write-Output "File(s) not deleted"
+                }
+            }
+            catch{
+                Write-Output $Error
+            }
+        }
+        else{
+            Write-Output $noFiles
+        }
+    }
+
+    else {
+        Write-Output $noFiles
+    }
+}
