92de75c79a69f826cad0d6704369d8967abc5a35
kolmstead17 <22797420+kolmstead17@users.noreply.github.com>
2024-12-06 09:32:59 -0500

    Added remote script

--- Windows_Event_Logs.ps1
+++ Windows_Event_Logs.ps1
@@ -1,2 +1,6 @@
+#Local script
 #Example log to show formatting
 Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational' -FilterXPath '*[System[EventID=1149]]' | Where-Object{$_.message -match 'putusernamehere'} | Format-List
+
+#Remote script
+Invoke-Command -ComputerName FQDNGoesHere -ScriptBlock {Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational' -FilterXPath '*[System[EventID=1149]]'} | Where-Object{$_.message -match 'putusernamehere'} | Format-List
